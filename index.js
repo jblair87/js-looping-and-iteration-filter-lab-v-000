4bdf57c5503ef4e152ac20373afccf58aea179db
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 17:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ return driverName.toLowerCase() === name.toLowerCase();
 }
 
 function fuzzyMatch(drivers){
-  
+ return list.filter(function (driverName) {
+return driverName.toLowerCase() === name.toLowerCase();
+});
 }
 
 function matchName(drivers){

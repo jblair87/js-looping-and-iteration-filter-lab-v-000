65808dccfdd171b9bd3b02100eb02623f67c8e6c
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 17:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMatching (list, name){
    return list.filter(function (driverName) {
-
+return driverName.toLowerCase() === name.toLowerCase();
    for (const user of drivers) {
      
      if user.firstName == 

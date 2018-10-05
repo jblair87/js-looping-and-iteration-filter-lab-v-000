286774ec768e5cb05d0ac0def99edfdd79539489
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 17:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 function findMatching (list, name){
-   return list.filter(function (driverName) {
+return list.filter(function (driverName) {
 return driverName.toLowerCase() === name.toLowerCase();
-   for (const user of drivers) {
-     
-     if user.firstName == 
-     console.log(user.firstName)}
+});
 }
-findMatching.users;
-
 
 function fuzzyMatch(drivers){
   
